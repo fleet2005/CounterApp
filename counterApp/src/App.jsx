23c5f95d1f3932aa82271b2b416f3a29d34400c3
fleet2005 <100420vishalsrinivasan.gbkm@gmail.com>
2025-06-13 00:06:07 +0530

    final commit

--- counterApp/src/App.jsx
+++ counterApp/src/App.jsx
@@ -14,11 +14,13 @@ const Counter = () => {
     }
 
     return (
-      <>
-          <button onClick={increment} className="button" id="one"> increment </button>
-          <div>{value}</div>
-          <button onClick={decrement} className="button" id="two"> decrement </button>
-      </>
+      <div className = "container">
+          <div id="value" className="topRow" style={{fontWeight : "bold", fontSize : "4vw"}}>{value}</div>
+          <div className = "bottomRow">
+            <button onClick={increment} className="button" id="one"> Increment </button>
+            <button onClick={decrement} className="button" id="two"> Decrement </button>
+          </div>
+      </div>
     )
 }
 
